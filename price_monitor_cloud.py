0e8852ac8e5c4dbf0d4dbb55fa4c9414d3850610
Marcos Reis <marcos.reis@nubank.com.br>
2025-08-05 15:30:47 -0300

    🔄 Forçar redeploy - Grupo Telegram configurado v3.1

--- price_monitor_cloud.py
+++ price_monitor_cloud.py
@@ -69,7 +69,7 @@ class StayCharliePriceMonitorCloud:
         self.price_history = self.load_price_history()
         self.units = self.get_enabled_units()
         
-        logger.info("🚀 StayCharlie Monitor Cloud iniciado - v3.0 - DEPLOY FORÇADO ÀS 19:10")
+        logger.info("🚀 StayCharlie Monitor Cloud iniciado - v3.1 - GRUPO TELEGRAM CONFIGURADO")
         enabled_units = [unit['name'] for unit in self.units]
         logger.info(f"🏠 Monitorando {len(enabled_units)} unidade(s):")
         for unit in self.units:
